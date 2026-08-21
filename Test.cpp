4eb161be86878a7fa262f5c93edfef137be853a9
MIFOOD <elinbronislav@gmail.com>
2026-08-21 17:00:07 +0300

    Answer to task_1

--- Test.cpp
+++ Test.cpp
@@ -1501,35 +1501,35 @@
 //     return 0;
 // }
 
-//Листинг 6.15 Использование вложенных циклов для вычисления чисел прогрессии Фибоначчи
-#include <iostream>
-using namespace std;
+// //Листинг 6.15 Использование вложенных циклов для вычисления чисел прогрессии Фибоначчи
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    const int NumsToCal = 5;
-    cout << "This program will calculate " << NumsToCal
-        <<" Fibonacci Numbers at a time" << endl;
+// int main()
+// {
+//     const int NumsToCal = 5;
+//     cout << "This program will calculate " << NumsToCal
+//         <<" Fibonacci Numbers at a time" << endl;
 
-    int Num1 = 0, Num2 = 1;
-    char WantMore = '\0';
-    cout << Num1 << " " << Num2 << " ";
+//     int Num1 = 0, Num2 = 1;
+//     char WantMore = '\0';
+//     cout << Num1 << " " << Num2 << " ";
 
-    do
-    {
-        for(int Index = 0; Index < NumsToCal; ++Index)
-        {
-            cout << Num1 + Num2 << " ";
+//     do
+//     {
+//         for(int Index = 0; Index < NumsToCal; ++Index)
+//         {
+//             cout << Num1 + Num2 << " ";
 
-            int Num2Temp = Num2;
-            Num2 = Num1 + Num2;
-            Num1 = Num2Temp;
-        }
-        cout << endl << "Do you want more numbers (y/n)? ";
-        cin >> WantMore;
-    }while(WantMore == 'y');
+//             int Num2Temp = Num2;
+//             Num2 = Num1 + Num2;
+//             Num1 = Num2Temp;
+//         }
+//         cout << endl << "Do you want more numbers (y/n)? ";
+//         cin >> WantMore;
+//     }while(WantMore == 'y');
     
-    cout << "Goodbye!" << endl;
+//     cout << "Goodbye!" << endl;
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ chapter6/task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 5;
+
+int main()
+{
+    int array[SIZE] = {1, 2, 3, 4, 5};
+
+    for(int i = SIZE -1; i >= 0; i--)
+        cout << array[i] << endl;
+
+    return 0;
+}
