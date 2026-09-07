5656cdbd5c2ce455a4513a7d677118c239ae4536
MIFOOD <elinbronislav@gmail.com>
2026-09-07 13:52:36 +0300

    Answer to task1 chapter12

---
+++ chapter12/task1.cpp
@@ -0,0 +1,33 @@
+// 1. Создайте оператор преобразования для класса Date, который преобразует содержа
+// щуюся в нем дату в целое число.
+#include <iostream>
+
+using namespace std;
+class Date
+{
+    private:
+    int day, month, year;
+    public:
+        Date(int DAY, int MONTH, int YEAR) : day(DAY), month(MONTH), year(YEAR){}
+
+        void showDate()
+        {
+            cout << day << " / " << month << " / " << year << endl;
+        }
+
+        operator int() const
+        {
+            return day + month + year;
+        }
+};
+
+int main()
+{
+    Date data(10, 10, 2011);
+
+    data.showDate();
+
+    int lol = (int)data;
+
+    return 0;
+}
