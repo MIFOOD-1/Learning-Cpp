95a7e81cf65fe026a84de77dc2fd6e0b2246291d
MIFOOD <elinbronislav@gmail.com>
2026-09-16 15:58:44 +0300

    Answer to task2 on Chapter18

---
+++ chapter18/task2.cpp
@@ -0,0 +1,44 @@
+// 2. На примере короткой программы продемонстрируйте, что итератор, указывающий 
+// на элемент в списке, продолжает оставаться допустимым, несмотря на то, что после 
+// или перед ним был вставлен другой элемент, изменив таким образом относитель
+// ную позицию прежнего элемента.
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+void inputList(list <int> &inputValue, int &value)
+{
+    inputValue.push_front(value);
+}
+
+void show_list(list <int> &inputValue) 
+{
+    for( auto iterator = inputValue.begin(); iterator != inputValue.end(); iterator++)
+        cout << *iterator << " ";
+}
+int main()
+{
+    std::list <int> some_object;
+    int some_num;
+    
+    cout << "vvedite skolko znacheni vi hotite vvesti: ";
+    int index = 0;
+    cin >> index;
+    
+    for(int i = 0; i < index; i++)
+    {   cout << "Vvedite znachenie kotoroe hotite dobavit: ";
+        cin >> some_num;
+        inputList(some_object, some_num);
+    }
+
+    show_list(some_object);
+
+
+    auto iterator = some_object.begin();
+    some_object.push_front(1);
+    cout << *iterator;
+    show_list(some_object);
+
+    return 0;
+}
