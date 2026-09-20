6fc1eeec75534b5ef4de4413070007d46399e3ab
MIFOOD <elinbronislav@gmail.com>
2026-09-20 13:42:03 +0300

    update answer to task1 in chapter20

--- chapter20/task1.cpp
+++ chapter20/task1.cpp
@@ -32,13 +32,13 @@ void DisplayContents(const T& Input)
 
 int main()
 {
-    multimap<int, book_phone> mmobject;
+    multimap<string, book_phone> mmobject;
     
-    mmobject.insert(make_pair(5, book_phone("Oleg", "+6 715 633 42 55")));
-    mmobject.insert(make_pair(2, book_phone("Danil", "+9 155 623 14 23")));
-    mmobject.insert(make_pair(3, book_phone("Roma", "+1 275 603 54 35")));
-    mmobject.insert(make_pair(2, book_phone("ALex", "+7 315 633 64 52")));
-    mmobject.insert(make_pair(10, book_phone("Oleg", "+2 495 623 44 15")));
+    mmobject.insert(make_pair("Oleg", book_phone("Oleg", "+6 715 633 42 55")));
+    mmobject.insert(make_pair("Danil", book_phone("Danil", "+9 155 623 14 23")));
+    mmobject.insert(make_pair("Roma", book_phone("Roma", "+1 275 603 54 35")));
+    mmobject.insert(make_pair("ALex", book_phone("ALex", "+7 315 633 64 52")));
+    mmobject.insert(make_pair("Oleg", book_phone("Oleg", "+2 495 623 44 15")));
 
     DisplayContents(mmobject);
 
